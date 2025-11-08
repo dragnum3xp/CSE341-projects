11d0e42dc316d3bdb732776d3962fcfb2e9722ab
Dragnum <dragnumexp@gmail.com>
2025-11-08 18:06:52 -0300

    Adding routes and swagger

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
+  //#swagger.tags=["Contacts"]
     const result = await mongodb.getDatabase().db("Project1").collection("contacts").find();
     result.toArray().then((contacts) => {
       res.setHeader("content-type", "application/json");
@@ -11,6 +12,7 @@ const getAll = async (req, res) => {
 
 
 const getSingle = async (req, res) => {
+  //#swagger.tags=["Contacts"]
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db("Project1").collection("contacts").find({ _id: contactId});
     result.toArray().then((contacts) => {
@@ -19,7 +21,57 @@ const getSingle = async (req, res) => {
     })
 };
 
+const createContact = async (req, res) => {
+  //#swagger.tags=["Contacts"]
+    const contact = {
+      firstName : req.body.firstName,
+      lastName : req.body.lastName,
+      email : req.body.email,
+      favoriteColor : req.body.favoriteColor,
+      birthDay : req.body.birthDay
+    };
+    const response = await mongodb.getDatabase().db("Project1").collection("contacts").insertOne(contact);
+    if (response.acknowledged > 0) {
+      res.status(204).send()
+    } else {
+      res.status(500).json(response.error || "Some error occurred while creating the contact");
+    }
+};
+
+const updateContact = async (req, res) => {
+  //#swagger.tags=["Contacts"]
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+      firstName : req.body.firstName,
+      lastName : req.body.lastName,
+      email : req.body.email,
+      favoriteColor : req.body.favoriteColor,
+      birthDay : req.body.birthDay
+    };
+    const response = await mongodb.getDatabase().db("Project1").collection("contacts").replaceOne({ _id: contactId}, contact);
+    if (response.modifiedCount > 0) {
+      res.status(204).send()
+    } else {
+      res.status(500).json(response.error || "Some error occurred while updating the contact");
+    }
+};
+
+const deleteContact = async (req, res) => {
+  //#swagger.tags=["Contacts"]
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db("Project1").collection("contacts").deleteOne({ _id: contactId});
+    if (response.deletedCount > 0) {
+      res.status(204).send()
+    } else {
+      res.status(500).json(response.error || "Some error occurred while deleting the contact");
+    }
+};
+
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -5,6 +5,14 @@ const contactsController = require("../controllers/contacts");
 
 router.get("/", contactsController.getAll);
 
-router.get("/:id", contactsController.getSingle)
+router.get("/:id", contactsController.getSingle);
+
+router.post("/", contactsController.createContact);
+
+router.put("/:id", contactsController.updateContact);
+
+router.delete("/:id", contactsController.deleteContact);
+
+
 
 module.exports = router;
